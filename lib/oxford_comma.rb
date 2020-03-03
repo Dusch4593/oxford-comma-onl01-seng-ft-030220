0917fa47cb65210846990abdc3f4f515c2a6daae
Brandon Dusch <brandondusch@gmail.com>
2020-03-03 13:10:47 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,7 +1,7 @@
 def oxford_comma(array)
   # Takes an array of striings
   if(array.size == 1) 
-    return array[0]
+    return array
   elsif(array.size == 2)
     array[1] = " and " + array[1]
     return array.join("")
