b432b5e28ae664a6f151b7680002893040b93a83
Brandon Dusch <brandondusch@gmail.com>
2020-03-03 13:06:59 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,18 +1,18 @@
 def oxford_comma(array)
   # Takes an array of striings
   def oxford_comma(array)
-  if(array.size == 1) 
-    return array.join("")
-  elsif(array.size == 2)
-    array[1] = "and " + array[1]
-    return array.join(", ")
-  end
-  for i in 0..array.size-1
-    if(i == array.size-1)
-      array[i] = "and " + array[i]
+    if(array.size == 1) 
+      return array.join("")
+    elsif(array.size == 2)
+      array[1] = "and " + array[1]
+      return array.join(", ")
     end
-  end
-  return array.join(", ")
+    for i in 0..array.size-1
+      if(i == array.size-1)
+        array[i] = "and " + array[i]
+      end
+    end
+    return array.join(", ")
   end
   
 end
